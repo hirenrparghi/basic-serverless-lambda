bf87f83c50611f5a18054aac03fa0d97dc73f0b1
Hiren Parghi <hirenparghi90@gmail.com>
2022-02-11 12:25:47 +0530

    12:25

--- user-change-password/handler.js
+++ user-change-password/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your functio1212n executed successfully!',
+        message: 'Go Serverless v1.0! Your functio1212n execute12d successfully!',
         input: event,
       },
       null,
